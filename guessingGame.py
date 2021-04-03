183d45895f67fd607e05a7664ff0321918a525b7
shreyashPramod <65612631+shreyashPramod@users.noreply.github.com>
2021-04-03 21:50:37 +0530

    Add files via upload

--- guessingGame.py
+++ guessingGame.py
@@ -1,9 +1,14 @@
-number=int(input("enter the number"))
+game=("Number Guessing Game")
+print(game)
+Guess=("Guess the Number between 1 to 9")
+print(Guess)
+number=int(input("enter your number"))
 print(number)
-if(number<9):
+if(number>9):
+    print("You Lose!!")
+    print("The Number is,9")
+elif(number>1):
     print("Congratulation You Won!!")
-elif(number>9):
-    print("You Lose!! The Number is",9)
 
 
 
